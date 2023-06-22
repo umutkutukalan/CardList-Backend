8722c6fec33e21dc0d0bec01c49663c0a56dd010
Umut Kütükalan <118997325+umutkutukalan@users.noreply.github.com>
2023-06-22 05:16:37 +0300

    BoşAlanKalamaz

--- CardList/src/main/java/com/proje/CardList/Service/impl/CardService.java
+++ CardList/src/main/java/com/proje/CardList/Service/impl/CardService.java
@@ -5,6 +5,9 @@ import com.proje.CardList.Entity.Card;
 import com.proje.CardList.Entity.Enum.Derece;
 import com.proje.CardList.Repository.CardRepository;
 import com.proje.CardList.Service.ICardService;
+
+import io.micrometer.common.util.StringUtils;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,10 +23,27 @@ public class CardService implements ICardService {
     public CardService(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
     }
+
     @Override
     public Card save(Card card) {
+        if (StringUtils.isEmpty(card.getAd())) {
+            throw new GeneralException("Görev adını giriniz!");
+        }
+        if (card.getDerece() == null) {
+            throw new GeneralException("Görev derecesini belirleyiniz!");
+        }
+        if (StringUtils.isEmpty(card.getTanim())) {
+            throw new GeneralException("Görev tanımını yapınız!");
+        }
+        if (StringUtils.isEmpty(card.getBasTarih())) {
+            throw new GeneralException("Lütfen başlangıç tarihini belirtiniz!");
+        }
+        if (StringUtils.isEmpty(card.getBitTarih())) {
+            throw new GeneralException("Görev tanımını yapınız!");
+        }
         return cardRepository.save(card);
     }
+
     @Override
     public Card getById(Long id) {
         Optional<Card> userOptional = cardRepository.findById(id);
@@ -32,17 +52,20 @@ public class CardService implements ICardService {
         }
         return userOptional.get();
     }
+
     @Override
     public List<Card> getAll() {
         return cardRepository.findAll();
     }
+
     @Override
     public Page<Card> getAll(Pageable pageable) {
         return cardRepository.findAll(pageable);
     }
+
     @Override
     public void delete(Long id) {
-        if (!cardRepository.existsById(id)){
+        if (!cardRepository.existsById(id)) {
             throw new GeneralException("Böyle bir görev kartı yok!");
         }
         cardRepository.deleteById(id);
